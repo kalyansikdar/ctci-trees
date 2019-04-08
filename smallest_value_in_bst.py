501b3abddcc83fe1eedf2e95759f0ea529ca06be
Kalyan Sikdar <kalyansikdar@users.noreply.github.com>
2019-04-07 20:07:53 -0500

    Update smallest_value_in_bst.py

--- smallest_value_in_bst.py
+++ smallest_value_in_bst.py
@@ -22,10 +22,16 @@ def get_tree(arr):
 
   return root
 
-def inorder(tree, k):
+def find_smallest_value(tree, k):
   while tree.left:
       tree = tree.left
   return (tree.val)
 
+def find_largest_value(tree, k):
+  while tree.right:
+      tree = tree.right
+  return (tree.val)
+
 sorted_array = [5,6,7,12,23,31,44,56,65,99, 100]
-print(inorder(get_tree(sorted_array), 3))
+print('Smallest Value:', find_smallest_value(get_tree(sorted_array), 3))
+print('Largest Value: ' , find_largest_value(get_tree(sorted_array), 3))
