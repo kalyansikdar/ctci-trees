370f6d6889401bf034973f3be92ccda7e4e65954
Kalyan Sikdar <kalyansikdar@users.noreply.github.com>
2019-04-07 20:21:37 -0500

    Update smallest_value_in_bst.py

--- smallest_value_in_bst.py
+++ smallest_value_in_bst.py
@@ -22,16 +22,37 @@ def get_tree(arr):
 
   return root
 
-def find_smallest_value(tree, k):
+def find_smallest_value(tree):
   while tree.left:
       tree = tree.left
   return (tree.val)
 
-def find_largest_value(tree, k):
+def find_largest_value(tree):
   while tree.right:
       tree = tree.right
   return (tree.val)
 
+
+##################################################################
+# This method can solve similar different problems
+##################################################################
+def _getSmallest(root, holder):
+    if not root:
+        return
+    else:
+        _getSmallest(root.left, holder)
+        holder.append(root.val)
+        _getSmallest(root.right, holder)
+
+def getSmallest(tree):
+    holder = []
+    _getSmallest(tree, holder)
+    print ('Holder: ', holder)
+    return holder[0]
+
 sorted_array = [5,6,7,12,23,31,44,56,65,99, 100]
-print('Smallest Value:', find_smallest_value(get_tree(sorted_array), 3))
-print('Largest Value: ' , find_largest_value(get_tree(sorted_array), 3))
+print('Get Smallest:' , getSmallest(get_tree(sorted_array)))
+sorted_array = [5,6,7,12,23,31,44,56,65,99, 100]
+print('Smallest Value:', find_smallest_value(get_tree(sorted_array)))
+print('Largest Value: ' , find_largest_value(get_tree(sorted_array)))
+
